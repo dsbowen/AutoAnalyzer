e0f3154c68aca1723abd12bc8f969da2af0514f1
dsbowen <dsbowen@wharton.upenn.edu>
2019-05-21 08:46:57 -0400

    cleaned writer

--- autoanalyzer/writer.py
+++ autoanalyzer/writer.py
@@ -1,7 +1,7 @@
 ##############################################################################
 # Table Writer
 # by Dillon Bowen
-# last modified 05/15/2019
+# last modified 05/21/2019
 ##############################################################################
 
 from autoanalyzer.table_generator import TableGenerator
@@ -9,6 +9,13 @@ from autoanalyzer.table import Table
 from autoanalyzer.bases.writer_base import WriterBase, POOLED_VAL
 import xlsxwriter
 
+'''
+Data:
+    file_name
+    worksheets: {ws title: [Worksheet, current row]}
+    tables: [Table or TableGenerator]
+    generated_tables: [Table (after generate)]
+'''
 class Writer(WriterBase):
     def __init__(self, file_name=None):
         self.file_name(file_name)
@@ -30,6 +37,7 @@ class Writer(WriterBase):
         self._wb.close()
         
     # Generate tables
+    # note that 'table' may be Table or TableGenerator
     def _generate_tables(self):
         self._generated_tables = []
         for table in self._tables:
@@ -38,13 +46,12 @@ class Writer(WriterBase):
             else:
                 self._generated_tables.append(table.generate())
         
-    # Initialize a new workbook to write
+    # Initialize a new workbook
+    # initialize workbook
+    # add formats
     def _init_workbook(self):
         self._wb = xlsxwriter.Workbook(self._file_name+'.xlsx')
-        self._add_formats()
-        
-    # Add formats to the workbook
-    def _add_formats(self):
+
         self._format = {}
         self._format['default'] = self._wb.add_format()
         self._format['bold'] = self._wb.add_format({'bold': True})
@@ -56,11 +63,14 @@ class Writer(WriterBase):
             format.set_align('vcenter')
            
     # Write a single table to the workbook
+    # if table belongs to new worksheet, initialize worksheet
+    # write table to worksheet
     def _write_table(self, table):
         ws_title = table._ws_title
         if ws_title not in self._worksheets:
             ws = self._wb.add_worksheet(ws_title)
             ws.set_column(0, 99, width=20)
             self._worksheets[ws_title] = [ws, 0]
+            
         self._worksheets[ws_title][1] = table._write(
             *self._worksheets[ws_title], self._format) - 1
